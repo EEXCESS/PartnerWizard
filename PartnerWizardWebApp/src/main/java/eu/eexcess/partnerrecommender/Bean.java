fcf461a574dd3921135003a84d83d2b72dc534c9
jr-dig-orgel <thomas.orgel@joanneum.at>
2015-10-21 14:33:39 +0200

    changed to java8 some minor layout changes

--- PartnerWizardWebApp/src/main/java/eu/eexcess/partnerrecommender/Bean.java
+++ PartnerWizardWebApp/src/main/java/eu/eexcess/partnerrecommender/Bean.java
@@ -39,7 +39,6 @@ import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
